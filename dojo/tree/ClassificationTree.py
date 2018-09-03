0bfc987d031b14631dab41c7e22b13b21ddc990f
Victor Velev <velev.victor@yahoo.com>
2018-09-03 22:37:21 +0100

    add classes Question, Node, Leaf + split and predict_one functions

--- dojo/tree/ClassificationTree.py
+++ dojo/tree/ClassificationTree.py
@@ -1,9 +1,15 @@
 from .utils import (
     np,
+
     BaseModel,
+
     gini_impurity,
     entropy,
     info_gain,
+
+    Question,
+    Node,
+    Leaf,
 )
 
 __all__ = [
@@ -11,5 +17,57 @@ __all__ = [
 ]
 
 class ClassificationTree(BaseModel):
-    pass
+    def __init__(self, metric="gini"):
+        self.metric = metric
+        self.root = None
+    
+    @staticmethod
+    def split(X, Y, question):
+        true_X, false_X = [], []
+        true_Y, false_Y = [], []
 
+        for x, y in X, Y:
+            if question.mark(x):
+                true_X.append(x)
+                true_Y.append(y)
+
+            else:
+                false_X.append(x)
+                false_Y.append(y)
+
+        return true_X, false_X, true_Y, false_Y
+
+    @staticmethod
+    def find_best_question(X, y):
+        current_impurity = gini_impurity(y)
+
+        for feature_n in range(X.shape[1]):
+            for value in X[:, feature_n]:
+                q = Question(feature_n, value)
+
+    def fit(self, X, y):
+        pass
+
+    @staticmethod
+    def predict_one(x, root):
+        if type(root) is Leaf:
+            return root.class_
+
+        if root.question.mark(x):
+            ClassificationTree.predict_one(x, root.true_branch)
+        else:
+            ClassificationTree.predict_one(x, root.false_branch)
+
+        return None
+
+    def predict(self, X):
+        return [ClassificationTree.predict_one(x, root) for x in X for root in [self.root]]
+
+    def predict_proba(self, X):
+        pass
+
+    def decision_function(self, X):
+        pass
+
+    def evaluate(self, X, y):
+        pass
--- dojo/tree/utils.py
+++ dojo/tree/utils.py
@@ -14,3 +14,25 @@ def entropy(s):
 
 def info_gain(current_impurity, impurity_after_action):
     return current_impurity - impurity_after_action
+
+class Question:
+    def __init__(self, feature_n, value):
+        self.feature_n = feature_n
+        self.value = value
+
+    def mark(self, x):
+        if type(self.value) is str:
+            return x[self.feature_n] == self.value
+        else:
+            return x[self.feature_n] >= self.value
+
+class Node:
+    def __init__(self, question=None, true_branch=None, false_branch=None):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+class Leaf:
+    def __init__(self, data):
+        self.data = data
+        self.class_ = max(set(data), key=data.count)
