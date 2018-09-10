a2dd24719dcf63ba55638a5b0208216f95e36f3d
Victor Velev <velev.victor@yahoo.com>
2018-09-10 18:44:06 +0300

    ExtraRegressionTree v1

---
+++ dojo/tree/ExtraRegressionTree.py
@@ -0,0 +1,55 @@
+from ..base import BaseModel
+from ..exceptions import MethodNotSupportedError
+
+from .utils.functions import build_extra_tree, tree_predict, print_tree
+from ..metrics.regression import mean_squared_error
+
+__all__ = [
+    "ExtraRegressionTree",
+]
+
+class ExtraRegressionTree(BaseModel):
+    """Extremely Randomized Regression Decision Tree model
+    
+    ... (more documentation)
+    
+    Parameters:
+    -----------
+    criterion : string, optional
+    max_depth : positive integer, optional
+    
+    """
+
+    def __init__(self, criterion="gini", max_depth=-1):
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.root = None
+
+    def fit(self, X, y):
+        X, y = super().fit(X, y)
+        self.root = build_extra_tree(
+            X, y,
+            self.criterion,
+            self.max_depth
+        )
+
+    def predict(self, X):
+        X = super().predict(X)
+        return [tree_predict(x, root, regression=True) for x in X for root in [self.root]]
+
+    def predict_proba(self, X):
+        raise MethodNotSupportedError("Probability predictions are not supported for Extra Regression Tree model.")
+
+    def decision_function(self, X):
+        raise MethodNotSupportedError("Decision function is not supported for Extra Regression Tree model.")
+
+    def evaluate(self, X, y):
+        X, y = super().evaluate(X, y)
+        print(
+            f"Mean Squared Error: {mean_squared_error(y, self.predict(X))}"
+        )
+
+    def visualize(self):
+        """Decision Tree visualization.
+        """
+        print_tree(self.root)
--- dojo/tree/__init__.py
+++ dojo/tree/__init__.py
@@ -2,10 +2,12 @@ from .ClassificationTree import ClassificationTree
 from .ExtraClassificationTree import ExtraClassificationTree
 
 from .RegressionTree import RegressionTree
+from .ExtraRegressionTree import ExtraRegressionTree
 
 __all__ = [
     "ClassificationTree",
     "ExtraClassificationTree",
     
     "RegressionTree",
+    "ExtraRegressionTree",
 ]
