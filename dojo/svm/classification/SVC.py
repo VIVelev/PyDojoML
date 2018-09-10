3f7c90d5ca1201f5d340f8baac2ab5215102a190
Victor Velev <velev.victor@yahoo.com>
2018-09-10 19:16:08 +0300

    SVC firts try

--- dojo/svm/classification/SVC.py
+++ dojo/svm/classification/SVC.py
@@ -1,5 +1,10 @@
 from .utils import (
     BaseModel,
+
+    svm_problem,
+    svm_parameter,
+    svm_train,
+    svm_predict,
 )
 
 __all__ = [
@@ -7,4 +12,50 @@ __all__ = [
 ]
 
 class SVC(BaseModel):
-    pass
+
+    def __init__(self, C=1.0, kernel="rbf", degree=3.0, gamma="auto"):
+        super().__init__()
+
+        self._estimator = None
+        self.C = C
+        if kernel.upper() == "LINEAR":
+            self.kernel = 0
+        elif kernel.upper() == "POLY":
+            self.kernel = 1
+        elif kernel.upper() == "SIGMOID":
+            self.kernel = 3
+        else:
+            self.kernel = 2 # RBF kernel
+        self.degree = degree
+        self.gamma = gamma
+
+    def fit(self, X, y):
+        X, y = super().fit(X, y)
+
+        if self.gamma.upper() == "AUTO":
+            self.gamma = 1.0/X.shape[0]
+
+        prob = svm_problem(y, X)
+        param = svm_parameter(
+            "-s 0 -c " + str(self.C) +
+            " -t " + str(self.kernel) +
+            " -d " + str(self.degree) +
+            " -g " + str(self.gamma)
+        )
+        self._estimator = svm_train(prob, param)
+        return self
+
+    def predict(self, X):
+        X = super().predict(X)
+
+        predictions, *_ = svm_predict([0 for _ in X], X, self._estimator)
+        return predictions
+
+    def predict_proba(self, X):
+        pass
+
+    def decision_function(self, X):
+        pass
+
+    def evaluate(self, X, y):
+        pass
--- dojo/svm/classification/utils.py
+++ dojo/svm/classification/utils.py
@@ -1,1 +1,8 @@
 from dojo.base import BaseModel
+
+from ..libsvm.svmutil import (
+    svm_problem,
+    svm_parameter,
+    svm_train,
+    svm_predict,
+)
