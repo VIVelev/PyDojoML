9faa1bbb1c03c624dba3d3062ac9b246cfed1732
Victor Velev <velev.victor@yahoo.com>
2018-09-10 20:24:40 +0300

    v1 of Regression Support Vector Machines, to be tested

--- dojo/svm/regression/SVR.py
+++ dojo/svm/regression/SVR.py
@@ -1,5 +1,14 @@
-from .utils import (
+from ..utils import (
     BaseModel,
+    MethodNotSupportedError,
+
+    set_kernel,
+    svm_problem,
+    svm_parameter,
+    svm_train,
+    svm_predict,
+
+    mean_squared_error,
 )
 
 __all__ = [
@@ -7,4 +16,59 @@ __all__ = [
 ]
 
 class SVR(BaseModel):
-    pass
+    """Epsilon-Support Vector Machine Regressor
+    
+    ... (more documentation)
+    
+    Parameters:
+    -----------
+    epsilon : float, optional
+    kernel : string, optional
+    degree : integer, optional
+    gamma : "auto" or float, optional
+    
+    """
+
+    def __init__(self, epsilon=0.01, kernel="rbf", degree=3, gamma="auto"):
+        super().__init__()
+
+        self._estimator = None
+        self.epsilon = epsilon
+        self.kernel = set_kernel(kernel)
+        self.degree = degree
+        self.gamma = gamma
+
+    def fit(self, X, y):
+        X, y = super().fit(X, y)
+
+        if self.gamma.upper() == "AUTO":
+            self.gamma = 1.0/len(X[0])
+
+        problem = svm_problem(y, X)
+        parameter = svm_parameter(
+            "-s 3 -p " + str(self.epsilon) +
+            " -t " + str(self.kernel) +
+            " -d " + str(self.degree) +
+            " -g " + str(self.gamma)
+        )
+        self._estimator = svm_train(problem, parameter)
+        return self
+
+    def predict(self, X):
+        X = super().predict(X)
+        predictions, *_ = svm_predict([0 for _ in X], X, self._estimator, options="-q")
+        return predictions
+
+    def predict_proba(self, X):
+        raise MethodNotSupportedError("Probability prediction are not supported for SVR model.")
+
+    def decision_function(self, X):
+        X = super().predict(X)
+        *_, decision_values = svm_predict([0 for _ in X], X, self._estimator, options="-q")
+        return decision_values
+
+    def evaluate(self, X, y):
+        X, y = super().evaluate(X, y)
+        print(
+            f"Mean Squared Error: {mean_squared_error(y, self.predict(X))}"
+        )
--- dojo/svm/utils.py
+++ dojo/svm/utils.py
@@ -1,13 +1,15 @@
-from dojo.base import BaseModel
+from ..base import BaseModel
+from ..exceptions import MethodNotSupportedError
 
-from ..libsvm.svmutil import (
+from .libsvm.svmutil import (
     svm_problem,
     svm_parameter,
     svm_train,
     svm_predict,
 )
 
-from ...metrics.classification import accuracy_score
+from ..metrics.classification import accuracy_score
+from ..metrics.regression import mean_squared_error
 
 def set_kernel(kernel_string):
     if kernel_string.upper() == "LINEAR":
