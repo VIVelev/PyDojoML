5762b3755aaf1f420eacc6d26baf5d4be1342163
Victor Velev <velev.victor@yahoo.com>
2018-09-19 23:26:44 +0300

    optimize code

--- dojo/dimred/utils.py
+++ dojo/dimred/utils.py
@@ -11,29 +11,31 @@ def get_mean_vectors(X, y):
     return [np.mean(X[y == cl, :], axis=0) for cl in y]
 
 def get_within_class_scatter_matrix(mean_vectors, X, y):
-    d = X.shape[1]
-    c = np.unique(y).size
-    Sw = np.zeros((d, d))
+    n_features = X.shape[1]
+    n_classes = np.unique(y).size
+    Sw = np.zeros((n_features, n_features))
 
-    for cl, m in zip(range(c), mean_vectors):
-        Si = np.zeros((d, d))
+    for cl, m in zip(range(n_classes), mean_vectors):
+        Si = np.zeros((n_features, n_features))
+        m = m.reshape(4, 1)
+
         for x in X[y == cl, :]:
-            x, m = x.reshape(4, 1), m.reshape(4, 1)
-            Si += (x - m) @ (x - m).T
-
+            v = (x.reshape(4, 1) - m)
+            Si +=  v @ v.T
         Sw += Si
+
     return Sw
 
 def get_between_class_scatter_matrix(mean_vectors, X, y):
-    d = X.shape[1]
-    c = np.unique(y).size
+    n_features = X.shape[1]
+    n_classes = np.unique(y).size
 
+    Sb = np.zeros((n_features, n_features))
     m = np.mean(X, axis=0).reshape(4, 1)
-    Sb = np.zeros((d, d))
 
-    for cl, m_i in zip(range(c), mean_vectors):
+    for cl, m_i in zip(range(n_classes), mean_vectors):
         n = X[y == cl, :].shape[0]
-        m_i = m_i.reshape(4, 1)
-        Sb += n * (m_i - m) @ (m_i - m).T
+        v = (m_i.reshape(4, 1) - m)
+        Sb += n * v @ v.T
 
     return Sb
