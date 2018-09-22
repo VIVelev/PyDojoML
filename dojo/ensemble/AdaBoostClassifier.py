92a92aa21736c8f29fd5f65f8bd520dd73e645b7
Victor Velev <velev.victor@yahoo.com>
2018-09-22 15:19:50 +0300

    finalize AdaBoost

--- dojo/ensemble/AdaBoostClassifier.py
+++ dojo/ensemble/AdaBoostClassifier.py
@@ -9,13 +9,20 @@ __all__ = [
 ]
 
 class AdaBoostClassifier(BaseModel):
+    """Adaptive Boost Classifier - ensemble method
     
-    def __init__(self, base_estimator=None, n_estimators=50, learning_rate=1.0):
-        self.base_estimator = base_estimator
-        self.n_estimators = n_estimators
-        self.learning_rate = learning_rate
+    Parameters:
+    -----------
+    base_classifier : dojo-classifier
+    n_classifiers : integer, optional
+    
+    """
+    
+    def __init__(self, base_classifier=None, n_classifiers=50):
+        self.base_classifier = base_classifier
+        self.n_classifiers = n_classifiers
 
-        self._estimators = [deepcopy(self.base_estimator) for _ in range(self.n_estimators)]
+        self._classifiers = [deepcopy(self.base_classifier) for _ in range(self.n_classifiers)]
 
     def fit(self, X, y):
         X, y = super().fit(X, y)
@@ -24,11 +31,11 @@ class AdaBoostClassifier(BaseModel):
         size = data.shape[0]
         idxs = list(range(size))
 
-        for i in range(self.n_estimators):
+        for i in range(self.n_classifiers):
             X, y = data[idxs, :-1], data[idxs, -1]
 
-            self._estimators[i].fit(X, y)
-            pred_y = np.array(self._estimators[i].predict(X))
+            self._classifiers[i].fit(X, y)
+            pred_y = np.array(self._classifiers[i].predict(X))
 
             data = np.vstack((
                 data,
@@ -47,8 +54,8 @@ class AdaBoostClassifier(BaseModel):
         predictions = []
         for x in X:
             current_predictions = []
-            for estimator in self._estimators:
-                current_predictions.append(estimator.predict([x])[0])
+            for classifier in self._classifiers:
+                current_predictions.append(classifier.predict([x])[0])
 
             predictions.append(
                 max(set(current_predictions), key=current_predictions.count)
@@ -58,11 +65,11 @@ class AdaBoostClassifier(BaseModel):
 
     def predict_proba(self, X):
         X = super().predict_proba(X)
-        return self._estimators[-1].predict_proba(X)
+        return self._classifiers[-1].predict_proba(X)
 
     def decision_function(self, X):
         X = super().decision_function(X)
-        return self._estimators[-1].decision_function(X)
+        return self._classifiers[-1].decision_function(X)
 
     def evaluate(self, X, y):
         X, y = super().evaluate(X, y)
