8c2d1729439c2ea8481ad7a583c2ae2644016069
Victor Velev <velev.victor@yahoo.com>
2018-09-23 22:38:03 +0300

    add verbose param to adaboost - debug option

--- dojo/ensemble/AdaBoostClassifier.py
+++ dojo/ensemble/AdaBoostClassifier.py
@@ -15,12 +15,14 @@ class AdaBoostClassifier(BaseModel):
     -----------
     base_classifier : dojo-classifier
     n_classifiers : integer, optional
+    verbose : boolean, optional
     
     """
     
-    def __init__(self, base_classifier=None, n_classifiers=50):
+    def __init__(self, base_classifier=None, n_classifiers=50, verbose=False):
         self.base_classifier = base_classifier
         self.n_classifiers = n_classifiers
+        self.verbose = verbose
 
         self._classifiers = [deepcopy(self.base_classifier) for _ in range(self.n_classifiers)]
 
@@ -32,20 +34,35 @@ class AdaBoostClassifier(BaseModel):
         idxs = list(range(size))
 
         for i in range(self.n_classifiers):
+            if self.verbose:
+                print("----------------------------------")
+                print("Iteration", i+1)
+                print("Training classifier...")
+
             X, y = data[idxs, :-1], data[idxs, -1]
-
             self._classifiers[i].fit(X, y)
-            pred_y = np.array(self._classifiers[i].predict(X))
 
+            if self.verbose:
+                print("Classifier", i+1, "trained.")
+                print("Weighting missclassified samples...")
+
+            pred_y = np.array(self._classifiers[i].predict(X))
             data = np.vstack((
                 data,
                 np.column_stack((
                     X[y != pred_y, :], y[y != pred_y]
                 ))
             ))
+
+            if self.verbose:
+                print("Shuffling the data...")
+
             np.random.shuffle(data)
             idxs = np.random.choice(list(range(data.shape[0])), size)
-
+            
+        if self.verbose:
+            print("=================================================================")
+            print("The training session has successfully finished.")
         return self
 
     def predict(self, X):
