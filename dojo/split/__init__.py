264841cf4d31650a7669cfe99e2673bd47d9691d
Victor Velev <velev.victor@yahoo.com>
2018-09-28 12:03:45 +0300

    add train_test_split / dedicate new module for data splitting and cv

---
+++ dojo/split/__init__.py
@@ -0,0 +1,5 @@
+from .train_test import train_test_split
+
+__all__ = [
+    "train_test_split",
+]
---
+++ dojo/split/train_test.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+__all__ = [
+    "train_test_split",
+]
+
+def train_test_split(X, y, test_size=0.3, shuffle=True):
+    """Train-test split
+    
+    Splits the data into a training and testing set.
+    
+    Parameters:
+    -----------
+    X : array-like, shape (n_samples, n_features)
+    The samples.
+    y : array-like, shape (n_samples, )
+    The labels.
+    test_size : float, from 0 to 1
+    The proportion of the data that is going to be
+    used for testing.
+    shuffle : boolean, whether to shuffle the data before
+    splitting it or not
+    
+    Returns:
+    --------
+    X_train : train-set, samples
+    y_train : train-set, labels
+    X_test : test-set, samples
+    y_test : test-set, labels
+    
+    """
+
+    X, y = np.array(X), np.array(y)
+    assert X.shape[0] == y.shape[0]
+
+    if shuffle:
+        data = np.column_stack((
+            X, y
+        ))
+
+        np.random.shuffle(data)
+        X, y = data[:, :-1], data[:, -1]
+
+    border = int((1 - test_size) * X.shape[0])
+    return X[:border, :], y[:border], X[border:, :], y[border:]
