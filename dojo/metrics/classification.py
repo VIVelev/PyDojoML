c998904e6965ba79b68a24d58804bd67d461f101
Victor Velev <velev.victor@yahoo.com>
2018-09-28 11:22:56 +0300

    implement true/false positives/negatives metrics

--- dojo/metrics/classification.py
+++ dojo/metrics/classification.py
@@ -1,8 +1,16 @@
-from .utils import np, convert_assert
+from .utils import (
+    np, convert_assert,
+    assert_binary_problem,
+)
 
 __all__ = [
     "accuracy_score",
     "accuracy",
+
+    "true_positives",
+    "false_positives",
+    "true_negatives",
+    "false_negatives",
 ]
 
 def accuracy_score(y, y_pred):
@@ -28,3 +36,27 @@ def accuracy_score(y, y_pred):
     return np.count_nonzero(y == y_pred) / y.size
 
 accuracy = accuracy_score
+
+def true_positives(y, y_pred):
+    y, y_pred = convert_assert(y, y_pred)
+    assert_binary_problem(y)
+
+    return np.count_nonzero(y_pred[y == 1] == 1)
+
+def false_positives(y, y_pred):
+    y, y_pred = convert_assert(y, y_pred)
+    assert_binary_problem(y)
+
+    return np.count_nonzero(y_pred[y == 0] == 1)
+
+def true_negatives(y, y_pred):
+    y, y_pred = convert_assert(y, y_pred)
+    assert_binary_problem(y)
+
+    return np.count_nonzero(y_pred[y == 0] == 0)
+
+def false_negatives(y, y_pred):
+    y, y_pred = convert_assert(y, y_pred)
+    assert_binary_problem(y)
+
+    return np.count_nonzero(y_pred[y == 1] == 0)
--- dojo/metrics/utils.py
+++ dojo/metrics/utils.py
@@ -1,7 +1,14 @@
 import numpy as np
+from ..exceptions import ParameterError
 
 def convert_assert(y, y_pred):
     y, y_pred = np.array(y), np.array(y_pred)
     assert y.size == y_pred.size
 
     return y, y_pred
+
+def assert_binary_problem(y):
+    if np.unique(y).size > 2:
+        raise ParameterError("The classification problem should be binary.")
+    else:
+        pass
