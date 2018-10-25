2c0538bb9ff21d2c64fcfe656c1eaa1058cd1b5f
Victor Velev <velev.victor@yahoo.com>
2018-10-26 00:50:54 +0300

    [Upgrade] Anom Detect Algo GD v4 (set parent class + implement methods)

--- dojo/anomaly/algorithms.py
+++ dojo/anomaly/algorithms.py
@@ -1,16 +1,20 @@
 import numpy as np
 from numpy import pi
 
+from ..base import BaseModel
+from ..exceptions import MethodNotSupportedError
+
 __all__ = [
     "GaussianDist",
 ]
 
-class GaussianDist:
+class GaussianDist(BaseModel):
+
     def __init__(self):
         self.mean = None
         self.std = None
 
-    def fit(self, X):
+    def fit(self, X, y=None):
         self.mean = np.mean(X, axis=0)
         self.std = np.std(X, axis=0)
 
@@ -21,6 +25,16 @@ class GaussianDist:
         assert len(X.shape) == 2
 
         return np.array([self.p(x) for x in X])
+
+    def predict_proba(self, X):
+        raise MethodNotSupportedError("""Probability predictions are not supported
+                                        for Gaussian Distribution Anomaly Detection.""")
+
+    def decision_function(self, X):
+        raise MethodNotSupportedError("Decision function is not supported. Use `predict` instead.")
+
+    def evaluate(self, X, y):
+        raise MethodNotSupportedError("Evaluation methods are not available yet...")
 
     def p(self, x):
         assert type(x) is list or type(x) is np.ndarray
