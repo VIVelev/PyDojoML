4b2359be870f05a63344f8c0a4fee2f43938487c
Victor Velev <velev.victor@yahoo.com>
2018-11-18 14:31:23 +0200

    [Update] implement idf v1

--- dojo/nlp/TF_IDF.py
+++ dojo/nlp/TF_IDF.py
@@ -20,13 +20,16 @@ class TF_IDF(BasePreprocessor):
         - k normalization, k = 0.5 by default ("k-norm")
 
     idf_weighting_scheme : string, optional
+    Supported weighting schemes:
+        - frequency ("freq")
+        - smooth frequency ("smooth")
     
     Returns:
     --------
     
     
     """
-    def __init__(self, tf_weighting_scheme="freq", idf_weighting_scheme=None, k=0.5):
+    def __init__(self, tf_weighting_scheme="freq", idf_weighting_scheme="freq", k=0.5):
         self.tf_weighting_scheme = tf_weighting_scheme.lower()
         self.idf_weighting_scheme = idf_weighting_scheme.lower()
         self.k = k
@@ -46,9 +49,14 @@ class TF_IDF(BasePreprocessor):
         else: # k-norm
             return self.k + (1-self.k) * raw_count / max(list(arr).count(x) for x in np.unique(arr))
 
-    @staticmethod
-    def _inverse_doc_frequency(cls, t, D):
-        pass
+    def _inverse_doc_frequency(self, t, D):
+        N = len(D)
+        nt = len([1 for doc in D if t in doc])
+
+        if self.idf_weighting_scheme == "freq":
+            return np.log(N / (nt+1))
+        else: # smooth
+            return np.log(1 + N / (nt+1))
 
     def fit(self, X):
         pass
