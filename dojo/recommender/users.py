12057d0be660e4f1c736607c234010268e67e64c
Victor Velev <velev.victor@yahoo.com>
2018-11-19 22:08:39 +0200

    [Update] encapsulate CBUser fields

--- dojo/recommender/users.py
+++ dojo/recommender/users.py
@@ -15,27 +15,27 @@ class CBUser:
         self._id = CBUser._id_counter
         CBUser._id_counter += 1
         
-        self.none_idxs = []
-        self.non_rated = []
-        self.model = None
+        self._none_idxs = []
+        self._non_rated = []
+        self._model = None
 
         self.fit(ratings)
 
     def fit(self, ratings):
         ratings = np.array(ratings)
 
-        self.none_idxs = np.where(ratings == None)
-        self.none_idxs = [self.none_idxs[i][0] for i in range(len(self.none_idxs))]
-        self.non_rated = [ratings[i, :] for i in self.none_idxs]
+        self._none_idxs = np.where(ratings == None)
+        self._none_idxs = [self._none_idxs[i][0] for i in range(len(self._none_idxs))]
+        self._non_rated = [ratings[i, :] for i in self._none_idxs]
 
-        ratings = [ratings[i, :] for i in range(ratings.shape[0]) if not i in self.none_idxs]
+        ratings = [ratings[i, :] for i in range(ratings.shape[0]) if not i in self._none_idxs]
         X, y = ratings[:, :-1], ratings[:, -1]
-        self.model = LinearRegression().fit(X, y)
+        self._model = LinearRegression().fit(X, y)
 
         return self
 
     def recommend(self):
-        pred = self.model.predict(self.non_rated)
+        pred = self._model.predict(self._non_rated)
         return {
-            self.none_idxs[i]: pred[i] for i in range(len(self.none_idxs))
+            self._none_idxs[i]: pred[i] for i in range(len(self._none_idxs))
         }
