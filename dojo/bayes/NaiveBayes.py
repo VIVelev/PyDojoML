cd5cb0d50bf833df993f8d4e742ab172708406c1
Victor Velev <velev.victor@yahoo.com>
2018-11-19 21:41:37 +0200

    [Fix] Naive Bayes repeated call of np.unique

--- dojo/bayes/NaiveBayes.py
+++ dojo/bayes/NaiveBayes.py
@@ -16,25 +16,27 @@ class NaiveBayes(BaseModel):
     
     """
 
-    def __init__(self, alpha=1e-9):
+    def __init__(self, alpha=1e-18):
         self.alpha = alpha
 
         self._X = []
         self._y = []
+        self._labels = []
 
     def fit(self, X, y):
         self._X, self._y = super().fit(X, y)
+        self._labels = np.unique(self._y)
         return self
 
     def predict(self, X):
         return np.array([
-            np.unique(self._y)[np.argmax(prob)] for prob in self.predict_proba(X)
+            self._labels[np.argmax(proba)] for proba in self.predict_proba(X)
         ])
 
     def predict_proba(self, X):
         X = super().predict_proba(X)
         return np.array([
-            [self.p(i, x) for i in np.unique(self._y)] for x in X
+            [self.p(label, x) for label in self._labels] for x in X
         ])
 
     def p(self, label, x):
@@ -42,7 +44,9 @@ class NaiveBayes(BaseModel):
         likelihood = np.count_nonzero(tmp == x) / tmp.shape[0]
 
         prior1 = np.count_nonzero(self._y == label) / self._y.size
-        prior2 = np.count_nonzero(self._X == x) / self._X.shape[0] + self.alpha
+        prior2 = np.count_nonzero(self._X == x) / self._X.shape[0]
+        if prior2 == 0:
+            prior2 += self.alpha
 
         return likelihood * prior1/prior2
 
