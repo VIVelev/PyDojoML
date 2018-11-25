bb5ba9e5c45c81dc5b98049fe32ed9ef98249e93
Victor Velev <velev.victor@yahoo.com>
2018-11-25 11:01:14 +0200

    [Update-doc] add __doc__ to cross validation

--- dojo/split/cross_validate.py
+++ dojo/split/cross_validate.py
@@ -7,10 +7,28 @@ __all__ = [
 
 
 def cross_validate(model, X, y, cv=5, metric=mean_squared_error):
+    """Cross Validation
+
+    Evaluates the given model using the given data
+    repetitively fitting and predicting on different
+    chunks (folds) from the data.
+    
+    Parameters:
+    -----------
+    model : dojo-model, the model to be evaluated
+    X : matrix, shape (n_samples, n_features), the data used for evaluation
+    y : vector, shape (n_samples, ), the desired labels
+    cv : integer, optional, the number of iterations
+    metric : the single value error/accuracy metric
+    
+    Returns:
+    --------
+    dict_results : dictionary with train score and test score
+    
+    """
+
     # TODO: Add feature that handles the metric function
     # automatically when metric="auto".
-
-    # TODO: Add docs.
 
     train_score = 0
     test_score = 0
