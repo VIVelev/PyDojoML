bb72f7ba5873353849adfab012b9a185d13853c3
Victor Velev <velev.victor@yahoo.com>
2018-11-29 01:45:39 +0200

    [Update] Model Stacking predict v1

--- dojo/ensemble/ModelStacking.py
+++ dojo/ensemble/ModelStacking.py
@@ -13,7 +13,6 @@ class ModelStacking(BaseModel):
         self.level_one_models = level_one_models
         self.level_two_models = level_two_models
 
-
     def fit(self, X, y):
         new_columns = []
         for i in range(len(self.level_one_models)):
@@ -25,10 +24,24 @@ class ModelStacking(BaseModel):
         ))
 
         for i in range(len(self.level_two_models)):
-            self.level_two_models.fit(X_final, y)
+            self.level_two_models[i].fit(X_final, y)
+
+        return self
 
     def predict(self, X):
-        pass
+        new_columns = []
+        for model in self.level_one_models:
+            new_columns.append(model.predict(X))
+
+        X_final = np.hstack((
+            X, np.transpose(new_columns)
+        ))
+
+        predictions = []
+        for model in self.level_two_models:
+            predictions.append(model.predict(X_final))
+        
+        return np.array(predictions)
 
     def predict_proba(self, X):
         pass
