ef18aeb3a1fbc8ceb28bfdb433c53d267fae98bd
Victor Velev <velev.victor@yahoo.com>
2018-12-17 19:27:16 +0200

    [Update] add l1 regularization + ab class regularizer

--- dojo/regularizers.py
+++ dojo/regularizers.py
@@ -1,11 +1,32 @@
+from abc import ABC, abstractmethod
 import numpy as np
 
 __all__ = [
+    "Regularizer",
+    "L1",
     "L2",
 ]
 
 
-class L2:
+class Regularizer(ABC):
+    @abstractmethod
+    def gradient(self, W):
+        pass
+
+# ====================================================================================================
+# ====================================================================================================
+
+class L1(Regularizer):
+    def __init__(self, lambd):
+        self.lambd = lambd
+
+    def __call__(self, W):
+        return self.lambd * np.sum(np.abs(W))
+
+    def gradient(self, W):
+        return self.lambd * np.abs(W)
+
+class L2(Regularizer):
     def __init__(self, lambd):
         self.lambd = lambd
 
