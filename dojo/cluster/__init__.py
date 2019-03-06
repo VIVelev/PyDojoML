d4cad42108ac08a4343d579c40611b5fe7ae345d
Victor Velev <velev.victor@yahoo.com>
2019-03-06 15:55:58 +0200

    [Update] add mixture clustering technique (GMM)

--- dojo/cluster/__init__.py
+++ dojo/cluster/__init__.py
@@ -1,7 +1,11 @@
+from .mixture import GaussianMixtureModel
+
 from .hierarchical import HierarchicalClustering
 from .kmeans import KMeans
 
 __all__ = [
+    "GaussianMixtureModel",
+
     "HierarchicalClustering",
     "KMeans",
 ]
---
+++ dojo/cluster/mixture/__init__.py
@@ -0,0 +1,5 @@
+from .gaussian_mixture_model import GaussianMixtureModel
+
+__all__ = [
+    "GaussianMixtureModel",
+]
